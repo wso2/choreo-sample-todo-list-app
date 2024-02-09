316230fc65cc149cab7cb9ce7a17fc00b2c601bb
Stefinie Fernando <minolispencer@gmail.com>
2024-02-09 12:47:04 +0530

    Add todo list nextjs web app

---
+++ app/api/todos/route.ts
@@ -0,0 +1,44 @@
+import { ToDo } from '@/types/todo';
+
+let todos: ToDo[] = [{
+  id: 1,
+  task: 'Buy Milk',
+  done: true
+}, {
+  id: 2,
+  task: 'Pay Utility Bills',
+  done: false
+}, {
+  id: 3,
+  task: 'Pick up Randy after Soccer',
+  done: false
+}];
+
+export async function GET() {
+  console.log('GET');
+  return Response.json(todos, { status: 200 });
+}
+
+export async function POST(req: Request) {
+  const { id, task } = await req.json();
+  const newTodo: ToDo = { id, task, done: false };
+  todos.push(newTodo);
+  return Response.json(newTodo, { status: 201 });
+}
+
+export async function DELETE(req: Request) {
+  const { id } = await req.json();
+  todos = todos.filter(todo => todo.id !== id);
+  return Response.json({ message: 'Todo deleted successfully' }, { status: 200 });
+}
+
+export async function PUT(req: Request) {
+  const { id } = await req.json();
+  const index = todos.findIndex(todo => todo.id === id);
+  if (index !== -1) {
+    todos[index].done = !todos[index].done;
+    return Response.json({ message: 'Todo updated successfully' }, { status: 200 });
+  } else {
+    return Response.json({ error: 'Todo not found' }, { status: 404 });
+  }
+}
---
+++ types/todo.ts
@@ -0,0 +1,6 @@
+export interface ToDo {
+    id: number;
+    task: string;
+    done?: boolean;
+  }
+  
